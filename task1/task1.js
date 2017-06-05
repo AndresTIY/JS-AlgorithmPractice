bf15ec3f12f0440511c1f875de3ca47a7257fd0d
Andres Cifuentes <aacifuentes@gmail.com>
2017-06-05 18:17:20 -0500

    created helper function, test passing

--- task1/task1.js
+++ task1/task1.js
@@ -1,5 +1,16 @@
 function isSwapped(A) {
-  return true;
+  if (isSorted(A)) {
+    return true;
+  }
+  return false;
+}
+function isSorted(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] > arr[i + 1]) {
+      return false;
+    }
+    return true;
+  }
 }
 
 module.exports = isSwapped;
--- task1/task1.spec.js
+++ task1/task1.spec.js
@@ -5,4 +5,8 @@ describe("isSwapped", function() {
     var result = isSwapped([1, 2, 3, 4]);
     expect(result).toEqual(true);
   });
+  it("second test", function() {
+    var result = isSwapped([1, 2, 4, 3]);
+    expect(result).toEqual(true);
+  });
 });
